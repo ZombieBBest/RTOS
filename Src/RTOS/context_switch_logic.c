458e3a0a660aa29b0f0b4dfb71cabac234f99d4e
ZombieBBest <savin.sa.2008@gmail.com>
2026-08-18 23:30:34 +0300

    Update context_switch_logic.c

--- Src/RTOS/context_switch_logic.c
+++ Src/RTOS/context_switch_logic.c
@@ -20,21 +20,3 @@ uint8_t* OS_SwitchLogic(uint8_t* current_task_stack_pointer) {
 
 	return chosen_task->stack_pointer;
 }
-
-/*
-uint8_t* OS_SwitchLogic(uint8_t* current_task_stack_pointer) {
-	OS_TCB_t** current_run_task = (OS_TCB_t**)&os_context.current_run_task;
-
-	(*current_run_task)->stack_pointer = current_task_stack_pointer;
-
-	uint32_t max_ready_priority = 31 - __builtin_clz(os_context.priority_bitmap);
-
-	OS_TCB_t* chosen_task = (OS_TCB_t*)os_context.task_ready_list[max_ready_priority];
-
-	*current_run_task = chosen_task;
-
-	os_context.task_ready_list[max_ready_priority] = chosen_task->next_node_ptr;
-
-	return chosen_task->stack_pointer;
-}
-*/
